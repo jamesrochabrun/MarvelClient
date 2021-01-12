eb5b9d09944345187a7b7e525b66d412d9b0bcc6
james rochabrun <jamesrochabrun@gmail.com>
2021-01-11 20:13:10 -0800

    SerieViewModel

--- Sources/MarvelClient/Models/Serie.swift
+++ Sources/MarvelClient/Models/Serie.swift
@@ -11,3 +11,18 @@ public struct Serie: Decodable {
     let title: String
     let thumbnail: Artwork
 }
+
+public class SerieViewModel: IdentifiableHashable, ObservableObject {
+    
+    public let id = UUID()
+    @Published var title: String
+    @Published var artwork: ArtworkViewModel
+    
+    public init(model: Serie) {
+        self.title = model.title
+        self.artwork = ArtworkViewModel(artWork: model.thumbnail)! // Should not be nil.
+    }
+}
+
+
+
